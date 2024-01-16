6ffdf59e9baf7127e2718459ff621971daee28e3
Branon Eusebio <branoneusebio@gmail.com>
2024-01-16 14:40:07 -0800

    Added about me section content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,18 +1,77 @@
 "use client";
 
-import { Flex, Heading, useColorModeValue } from "@chakra-ui/react";
+import { Flex, Box, Heading, useColorModeValue, Text } from "@chakra-ui/react";
 import { PageWrapper } from "@/components/PageWrapper";
+import { FancyHeading } from "@/components/FancyHeading";
+import { Photography } from "@/components/sections/Photography";
 
 export default function Home() {
   const bg = useColorModeValue("white", "brand.grey");
-
+  const cardBg = useColorModeValue("brand.lightBg", "brand.darkBg");
+  const shadow = useColorModeValue(
+    "lg",
+    "0px 4px 15px 0px rgba(226,175,255, 0.10)"
+  );
   return (
-    <PageWrapper bg={bg}>
-      <Flex width="100%" alignItems="flex-start" justifyContent="flex-start">
-        <Heading as="h1" size="2xl">
+    <PageWrapper bg={bg} pb={8}>
+      <Flex
+        width="100%"
+        alignItems="flex-start"
+        justifyContent="flex-start"
+        flexDirection="column"
+      >
+        <Heading as="h1" size="2xl" mb={4}>
           About Me
         </Heading>
-        <Heading as="h2"></Heading>
+        <FancyHeading as="h2">I love people. I love technology.</FancyHeading>
+        <Text as="p" mb={4}>
+          My love of people and my interest in the human experience are the
+          driving forces in my life. That's why I got my degree in neuroscience
+          and spent my first few years out of college as the director of a
+          neuropsychiatric clinic.
+        </Text>
+        <Text as="p" mb={4}>
+          As my fascination with technology got the better of me, I began to
+          experiment with designing and coding websites. I was hooked
+          immediately, and since then I've been diving deeper and deeper into
+          the vast worlds of software development and UX/UI design. Nowadays, I
+          specialize in design systems, accessibility, prototyping, and general
+          frontend engineering. I love flexing creativity and systems-level
+          thinking in building beautiful and intuitive interfaces, and in
+          constructing world-class design systems + component libraries.
+        </Text>
+        <Text as="p" mb={4}>
+          Outside of my main roles, I build productivity tools in my startup,
+          LIFTOFF, co-create Udemy software development courses for tens of
+          thousands of students, freelance as a web designer and developer for
+          small businesses, and much more!
+        </Text>
+
+        <Box
+          width="100%"
+          borderRadius={10}
+          padding={4}
+          mt={8}
+          boxShadow={shadow}
+          bg={cardBg}
+        >
+          <Heading
+            color="brand.blue"
+            as="h3"
+            size="l"
+            borderLeft="2px solid"
+            pl={2}
+            mb={4}
+          >
+            I'm more than just a tech worker though!
+          </Heading>
+          <Text>
+            Life's too short to do anything but what you love. Here are some of
+            my other passions:
+          </Text>
+        </Box>
+
+        <Photography />
       </Flex>
     </PageWrapper>
   );
---
+++ src/components/sections/Photography.tsx
@@ -0,0 +1,42 @@
+import { Flex, Grid, Image, Box, useColorModeValue } from "@chakra-ui/react";
+
+export const Photography = () => {
+  const photos: string[] = [
+    "/photography/bug.png",
+    "/photography/harbor.png",
+    "/photography/jellybowl.png",
+    "/photography/mammoth.png",
+    "/photography/matt.png",
+    "/photography/pismo.png",
+    "/photography/rach.png",
+    "/photography/sd.png",
+    "/photography/tahoe.png",
+  ];
+  const border = useColorModeValue("brand.lightBg", "brand.darkBg");
+  const shadow = useColorModeValue(
+    "lg",
+    "0px 4px 15px 0px rgba(226,175,255, 0.10)"
+  );
+
+  return (
+    <Flex my={4} width="100%">
+      <Grid
+        width="100%"
+        gridGap={4}
+        gridTemplateColumns={["1fr", "1fr 1fr", "1fr 1fr 1fr"]}
+      >
+        {photos.map((photo) => (
+          <Box
+            overflow="hidden"
+            borderRadius={16}
+            boxShadow={shadow}
+            border="8px solid"
+            borderColor={border}
+          >
+            <Image src={photo} minWidth="100%" objectFit="cover" />
+          </Box>
+        ))}
+      </Grid>
+    </Flex>
+  );
+};
