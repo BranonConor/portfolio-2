719912b497a9e98334e320890c2b7fdd613c3506
Branon Eusebio <branoneusebio@gmail.com>
2024-04-27 11:34:51 -0700

    Added filters to the engagements page

---
+++ src/app/engagements/consts.ts
@@ -0,0 +1,33 @@
+export const publicSpeaking = [
+  {
+    title: "Design Systems Panel + Brunch 🥞",
+    category: "Mentoring",
+    image: "/engagements/design-systems-brunch/cover.png",
+    link: "/engagements/posts/design-systems-brunch",
+    date: "April 2022",
+  },
+  {
+    title: "Featured Guest @ One Bad Habit Podcast",
+    category: "Public Speaking",
+    image: "/engagements/one-bad-habit/cover.png",
+    link: "/engagements/posts/one-bad-habit",
+    date: "June 2022",
+  },
+  {
+    title: "Design Panel @ SDSU + Friends of Figma",
+    category: "Mentoring",
+    image: "/engagements/sdsu-friends-of-figma/cover.png",
+    link: "/engagements/posts/sdsu-friends-of-figma",
+    date: "April 2022",
+  },
+];
+
+export const mentoring = [
+  {
+    title: "Mentoring on ADPList",
+    category: "Mentoring",
+    image: "/engagements/adplist/cover.png",
+    link: "/engagements/posts/adplist",
+    date: "Feb 2024 - Now",
+  },
+];
--- src/app/engagements/page.tsx
+++ src/app/engagements/page.tsx
@@ -1,42 +1,16 @@
 "use client";
 
-import { Flex, Grid, Heading, useColorModeValue } from "@chakra-ui/react";
+import { Flex, Text, Grid, Heading, useColorModeValue } from "@chakra-ui/react";
 import { PageWrapper } from "@/components/PageWrapper";
 import { FancyHeading } from "@/components/FancyHeading";
 import { PostCard } from "@/components/blog/PostCard";
+import { useState } from "react";
+import { mentoring, publicSpeaking } from "./consts";
+import { motion } from "framer-motion";
 
 export default function Home() {
   const bg = useColorModeValue("white", "brand.grey");
-  const engagements = [
-    {
-      title: "Design Systems Panel + Brunch 🥞",
-      category: "Mentoring",
-      image: "/engagements/design-systems-brunch/cover.png",
-      link: "/engagements/posts/design-systems-brunch",
-      date: "April 2022",
-    },
-    {
-      title: "Mentoring on ADPList",
-      category: "Mentoring",
-      image: "/engagements/adplist/cover.png",
-      link: "/engagements/posts/adplist",
-      date: "Feb 2024 - Now",
-    },
-    {
-      title: "Featured Guest @ One Bad Habit Podcast",
-      category: "Public Speaking",
-      image: "/engagements/one-bad-habit/cover.png",
-      link: "/engagements/posts/one-bad-habit",
-      date: "June 2022",
-    },
-    {
-      title: "Design Panel @ SDSU + Friends of Figma",
-      category: "Mentoring",
-      image: "/engagements/sdsu-friends-of-figma/cover.png",
-      link: "/engagements/posts/sdsu-friends-of-figma",
-      date: "April 2022",
-    },
-  ];
+  const [currentFilter, setCurrentFilter] = useState("all");
 
   return (
     <PageWrapper bg={bg}>
@@ -50,22 +24,132 @@ export default function Home() {
           Engagements 🤝
         </Heading>
         <FancyHeading as="h2">Showing up for the community!</FancyHeading>
-        <Grid
-          width="100%"
-          gridGap={4}
-          mb={8}
-          gridTemplateColumns={["1fr", "1fr", "1fr 1fr", "1fr 1fr", "1fr 1fr"]}
-        >
-          {engagements.map((engagement) => (
-            <PostCard
-              title={engagement.title}
-              image={engagement.image}
-              link={engagement.link}
-              category={engagement.category}
-              date={engagement.date}
-            />
-          ))}
-        </Grid>
+
+        <Flex flexWrap="wrap" mb={4}>
+          <Heading as="h3" m={0} mr={2} p={0} size="md" width="auto">
+            Filters:
+          </Heading>
+          <Text
+            color="white"
+            as="button"
+            fontSize="12px"
+            paddingY={1}
+            paddingX={2}
+            bg={currentFilter === "all" ? "brand.pink" : "brand.blue"}
+            borderRadius={120}
+            onClick={() => setCurrentFilter("all")}
+            mr={2}
+            mb={2}
+          >
+            All
+          </Text>
+          <Text
+            color="white"
+            as="button"
+            fontSize="12px"
+            paddingY={1}
+            paddingX={2}
+            bg={
+              currentFilter === "Public Speaking" ? "brand.pink" : "brand.blue"
+            }
+            borderRadius={120}
+            onClick={() => setCurrentFilter("Public Speaking")}
+            mr={2}
+            mb={2}
+          >
+            🎙️ Public Speaking
+          </Text>
+          <Text
+            color="white"
+            as="button"
+            fontSize="12px"
+            paddingY={1}
+            paddingX={2}
+            bg={currentFilter === "Mentoring" ? "brand.pink" : "brand.blue"}
+            borderRadius={120}
+            onClick={() => setCurrentFilter("Mentoring")}
+            mr={2}
+            mb={2}
+          >
+            🤝 Mentoring
+          </Text>
+        </Flex>
+
+        {(currentFilter === "all" || currentFilter === "Mentoring") && (
+          <motion.div
+            initial={{ x: -32, scale: 0.95, opacity: 0 }}
+            animate={{ x: 0, scale: 1, opacity: 1 }}
+            transition={{
+              duration: 0.15,
+              type: "spring",
+            }}
+            style={{ width: "100%" }}
+          >
+            <Heading as="h3" size="md" my={4}>
+              🤝 Mentoring
+            </Heading>
+            <Grid
+              width="100%"
+              gridGap={4}
+              mb={8}
+              gridTemplateColumns={[
+                "1fr",
+                "1fr",
+                "1fr 1fr",
+                "1fr 1fr",
+                "1fr 1fr",
+              ]}
+            >
+              {mentoring.map((engagement) => (
+                <PostCard
+                  title={engagement.title}
+                  image={engagement.image}
+                  link={engagement.link}
+                  category={engagement.category}
+                  date={engagement.date}
+                />
+              ))}
+            </Grid>
+          </motion.div>
+        )}
+
+        {(currentFilter === "all" || currentFilter === "Public Speaking") && (
+          <motion.div
+            initial={{ x: -32, scale: 0.95, opacity: 0 }}
+            animate={{ x: 0, scale: 1, opacity: 1 }}
+            transition={{
+              duration: 0.15,
+              type: "spring",
+            }}
+            style={{ width: "100%" }}
+          >
+            <Heading as="h3" size="md" my={4}>
+              🎙️ Public Speaking
+            </Heading>
+            <Grid
+              width="100%"
+              gridGap={4}
+              mb={8}
+              gridTemplateColumns={[
+                "1fr",
+                "1fr",
+                "1fr 1fr",
+                "1fr 1fr",
+                "1fr 1fr",
+              ]}
+            >
+              {publicSpeaking.map((engagement) => (
+                <PostCard
+                  title={engagement.title}
+                  image={engagement.image}
+                  link={engagement.link}
+                  category={engagement.category}
+                  date={engagement.date}
+                />
+              ))}
+            </Grid>
+          </motion.div>
+        )}
       </Flex>
     </PageWrapper>
   );
